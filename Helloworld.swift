2c8eff4f49183d501aef6f9c04ddad91bae38bf2
Santhosh Gandekattera <sgandekattera@innominds.com>
2018-11-09 15:43:45 +0530

    first commit

---
+++ Helloworld.swift
@@ -0,0 +1,19 @@
+//
+//  Helloworld.swift
+//  TestPods
+//
+//  Created by Santhosh Gandekattera on 09/11/18.
+//  Copyright © 2018 Santhosh Gandekattera. All rights reserved.
+//
+
+import Foundation
+
+class Helloworld{
+
+    func getMessage() -> String {
+        return "Hello World from Pod File!!!"
+        
+    }
+}
+
+
